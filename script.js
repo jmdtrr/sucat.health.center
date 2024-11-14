3ebf48dbfa89ef5ccf99a2727cee7d43ebf9e0a1
jmdtrr <jmdtrr@gmail.com>
2024-11-14 23:30:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ function goToRegister() {
     window.location.href = 'register.html'; 
 }
 function goToLogin() {
+    window.location.href = 'login.html'; 
+}
+function goToAppointment() {
     window.location.href = 'appointment.html'; 
 }
-
-
-
-
+function goToDoctorLogin() {
+    window.location.href = 'doctorLogin.html'; 
+}
+function goToDoctorRegister() {
+    window.location.href = 'doctorRegister.html'; 
+}
+function goToDashboard() {
+    window.location.href = 'dashboard.html'; 
+}
